30513900afb2d7e88cd8d69a17a35a13d5cb155d
Azempresas <azempresas@gmail.com>
2023-04-23 19:43:47 -0300

    do computador
    iniciando o menu responsivo

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
 //Criando um novo menu login 
 
-const pegaMenu = document.getElementById('menu');
+const pegaMenu = document.querySelector('.menu');
 const criaLi = document.createElement('li');
 
 
 pegaMenu.appendChild(criaLi);
 criaLi.innerHTML = 'Login'
 criaLi.classList.add('login');
-
-
-
 
 // ---------------------------
 
